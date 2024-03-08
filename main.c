339e85e5a78ae8c2b1484c01a1d5cada59b0807d
benyia <120672130+benyia@users.noreply.github.com>
2024-03-08 20:42:40 +0800

    1-12  DS1302的基本应用

--- main.c
+++ main.c
@@ -1,132 +1,112 @@
-#include <reg52.H>
-//#include <absacc.h>
-#include <onewire.h>
+#include "reg52.h"
+#include "ds1302.h"
 
-//-------å…±é˜³æ•°ç ç®¡çš„æ®µç ç¼–ç è¡¨ï¼ˆæ— å°æ•°ç‚¹ï¼‰--------
-unsigned char code SMG_NoDot[18]={0xc0,0xf9,//0-1
-    0xa4,0xb0,0x99,0x92,0x82,0xf8,0x80,0x90,//2-9
-    0x88,0x80,0xc6,0xc0,0x86,0x8e,//A-F
-	0xbf,0x7f};//- .
-//-------å…±é˜³æ•°ç ç®¡çš„æ®µç ç¼–ç è¡¨ï¼ˆå¸¦å°æ•°ç‚¹ï¼‰--------
-//0.ï½9.
-unsigned char code SMG_Dot[10]={0x40,0x79,
-    0x24,0x30,0x19,0x12,0x02,0x78,0x00,0x10};
-
+typedef unsigned int uint;
 typedef unsigned char uchar;
-typedef unsigned int uint;	
 
-uint temp=0;
+uchar Write_DS1302_adrr[7] = {0X80, 0X82, 0X84, 0X86, 0X88, 0X8A, 0X8C};
+uchar Read_DS1302_adrr[7] = {0X81, 0X83, 0X85, 0X87, 0X89, 0X8B, 0X8D};
+uchar Timer[7] = {0X00, 0X00, 0X00, 0X18, 0X04, 0X06, 0X20};
+
+uchar code SMG_duanma[18] = 
+	{0xc0,0xf9,0xa4,0xb0,0x99,0x92,0x82,0xf8,0x80,0x90,
+	 0x88,0x80,0xc6,0xc0,0x86,0x8e,
+	 0xbf,0x7f};//·Ö±ğÊÇ0-9£¨¶ÔÓ¦ÏÂ±ê£©£¬A-F£¬¡°-¡±£¬¡°.¡±
+
+void DS1302_Config()
+{
+	char i;
 	
+	Write_Ds1302_Byte(0X8E, 0X00);  //´ò¿ª±£»¤
+	for(i = 0; i < 7; i++)
+	{
+		Write_Ds1302_Byte(Write_DS1302_adrr[i], Timer[i]);
+	}
+
+}
+
+void Read_DS1302_Timer()
+{
+	uchar i;
+	for(i = 0; i < 7; i++)
+	{
+		Timer[i] = Read_Ds1302_Byte(Read_DS1302_adrr[i]);
+	}
+}
+
+//ÅäÖÃHC138
 void SelectHC138(uchar channel,uchar dat)
 {
-	P0 = dat;                      //å¾…è®¾ç½®æ•°æ®
+	P0=dat;
 	switch(channel)
 	{
-		case 4:
-			P2=(P2&0x1f)|0x80;
+		case 4:    //LED
+			P2 = (P2 & 0X1F) | 0X80;
 		break;
-		case 5:
-			P2=(P2&0x1f)|0xa0;
+		case 5:    //·äÃùÆ÷ºÍ¼ÌµçÆ÷
+			P2 = (P2 & 0X1F) | 0Xa0;
 		break;
-		case 6:
-			P2=(P2&0x1f)|0xc0;
+		case 6:    //Î»Âë
+			P2 = (P2 & 0X1F) | 0Xc0;
 		break;
-		case 7:
-			P2=(P2&0x1f)|0xe0;
+		case 7:    //¶ÎÂë
+			P2 = (P2 & 0X1F) | 0Xe0;
 		break;
-		case 0:
-			P2=(P2&0x1f)|0x00;
+		case 0:    //Ëø×¡ËùÓĞ¼Ä´æÆ÷
+			P2 = (P2 & 0X1F) | 0X00;
 		break;
 	}
-	P2 = (P2 & 0x1f) | 0x00;      //è®¾ç½®å®Œæˆï¼Œå…³é—­å…¨éƒ¨é”å­˜å™¨
-}	
-
-void Delay(uint t)
-{
-	while(t--);
-}
-
-void DisPlaySMG_Bit(uchar pos,uchar value)
-{
-	SelectHC138(6,0x01<<pos);
-	SelectHC138(7,value);
-	Delay(500);                //æ•°ç ç®¡ç‚¹äº®æ—¶é•¿
-	SelectHC138(6, 0x01 << pos);        
-	SelectHC138(7, 0xff);            //æ¶ˆéš
-}
-void DisPlay_All(uchar dat)
-{
-	SelectHC138(6,0xff);
-	SelectHC138(7,dat);
-}
-void DisPlaySMG_Temp()
-{
-	DisPlaySMG_Bit(7,SMG_NoDot[temp%10]);
-	DisPlaySMG_Bit(6,SMG_Dot[(temp/10%10)]);
-	DisPlaySMG_Bit(5,SMG_NoDot[temp/100]);
-	
-	DisPlaySMG_Bit(4,0xff);
-	DisPlaySMG_Bit(3,0xff);
-	DisPlaySMG_Bit(2,0xff);
-	
-	DisPlaySMG_Bit(1,0xff);
-	DisPlaySMG_Bit(0,0xff);
-	
-	DisPlay_All(0xff);
-	
-}
-void Delay_temp(uint t)
-{
-	while(t--)
-	{
-		DisPlaySMG_Temp();
-	}
-}
-void Read_DS18B20_temp()
-{
-	uchar LSB,MSB;
-	
-	init_ds18b20();
-	Write_DS18B20(0xcc);
-	Write_DS18B20(0x44);
-	Delay_temp(200);
-	
-	init_ds18b20();
-	Write_DS18B20(0xcc);
-	Write_DS18B20(0xBE);
-	
-	LSB=Read_DS18B20();
-	MSB=Read_DS18B20();
-//	init_ds18b20();
-	
-//	temp=MSB;
-//	temp<<=8;
-//	temp=temp|LSB;
-	
-	temp = MSB;                  //åˆæˆ16ä½æ¸©åº¦åŸå§‹æ•°æ®
-	temp = ((temp << 8) | LSB);
-	
-	if((temp&0xf800)==0x0000)
-	{
-		temp>>=4;
-		temp = temp*10;
-		temp = temp+(LSB&0x0f)*0.625;
-	}
-	
+	P2 = (P2 & 0X1F) | 0X00;
 }
 
 void Initsys()
 {
-	SelectHC138(4,0xFF);
-	SelectHC138(5,0x00);
+	SelectHC138(5,0X00);
+	SelectHC138(4,0xff);
 	SelectHC138(0,0x00);
+	
 }
+
+
+
+void Delay_SMG(uint t)
+{
+	while(t--);
+}
+
+//ÔÚposÎ»ÂëÉÏ£¬ÏÔÊ¾value¶ÎÂë
+void DisplaySMG_Bit(uchar pos, uchar value)
+{
+	SelectHC138(6,0x01<<pos);
+	SelectHC138(7,value);
+	Delay_SMG(500);                //ÊıÂë¹ÜµãÁÁÊ±³¤
+	SelectHC138(6, 0x01 << pos);        
+	SelectHC138(7, 0xff);            //ÏûÒş
+}
+
+void Display_DS1302()
+{
+	DisplaySMG_Bit(0, SMG_duanma[Timer[2]/16]);
+	DisplaySMG_Bit(1, SMG_duanma[Timer[2]%16]);
+	
+	DisplaySMG_Bit(2, SMG_duanma[16]);
+	
+	DisplaySMG_Bit(3, SMG_duanma[Timer[1]/16]);
+	DisplaySMG_Bit(4, SMG_duanma[Timer[1]%16]);
+	
+	DisplaySMG_Bit(5, SMG_duanma[16]);
+	
+	DisplaySMG_Bit(6, SMG_duanma[Timer[0]/16]);
+	DisplaySMG_Bit(7, SMG_duanma[Timer[0]%16]);
+}
+
 void main()
 {
 	Initsys();
+	DS1302_Config();
 	while(1)
 	{
-		Read_DS18B20_temp();
-		DisPlaySMG_Temp();
+		Read_DS1302_Timer();
+		Display_DS1302();
 	}
 }
