73868efebe2efe4fee0c8e84a389271139d40c79
benyia <120672130+benyia@users.noreply.github.com>
2024-03-08 20:31:03 +0800

    1-5  独立按键的基本操作

--- main.c
+++ main.c
@@ -1,108 +1,82 @@
-#include <REGX52.H>
+#include "reg52.h"  
 
 #define Init_138(n) P2=(P2&0x1f)|n<<5;
+
+sbit HC138_A = P2^5;     
+sbit HC138_B = P2^6; 
+sbit HC138_C = P2^7;       
 
 sbit S7 = P3^0;        
 sbit S6 = P3^1;        
 sbit S5 = P3^2;        
 sbit S4 = P3^3;        
 
-sbit L1 = P0^0;
-sbit L2 = P0^1;
-sbit L3 = P0^2;
-sbit L4 = P0^3;
-sbit L5 = P0^4;
-sbit L6 = P0^5;
-	
-void DelayK(unsigned char t)
+unsigned char code SMG_NoDot[18] = 
+    {0xc0,0xf9,0xa4,0xb0,0x99,0x92,0x82,0xf8,
+     0x80,0x90,0x88,0x80,0xc6,0xc0,0x86,0x8e,
+     0xbf,0x7f};
+unsigned char code SMG_HaveDot[16]=
+	{0x40,0x79,0x24,0x30,0x19,0x12,0x02,0x78,
+	0x00,0x10,0x08,0x03,0x46,0x21,0x06,0x0e};
+void Delay(unsigned char t)
 {
         while(t--);
 }
 
-unsigned char stat_k=0;
+
+
+void ShowKeyNum(unsigned char value)
+{
+        Init_138(6);
+        P0 = 0x01;
+        Init_138(7);
+        P0 = value;
+}
+
 void ScanKeys()
 {
-	if(S7 == 0)
-	{
-		DelayK(100);
-		if(S7 == 0)
-		{
-			if(stat_k == 0)
-			{
-				L1 = 0;
-				stat_k = 1;
-			}
-			else if(stat_k == 1)
-			{
-				L1 = 1;
-				stat_k = 0;
-			}
-			while(S7 == 0);
-		}
-	}
-	if(S6 == 0)
-	{
-		DelayK(100);
-		if(S6 == 0)
-		{
-			if(stat_k == 0)
-			{
-				L2 = 0;
-				stat_k = 2;
-			}
-			else if(stat_k == 2)
-			{
-				L2 = 1;
-				stat_k = 0;
-			}
-			while(S6 == 0);
-		}
-	}
-	if(S5 == 0)
-	{
-		DelayK(100);
-		if(S5 == 0)
-		{
-			if(stat_k == 1)
-			{
-				L3 = 0;
-				while(S5 == 0);
-				L3 = 1;
-			}
-			else if(stat_k == 2)
-			{
-				L5 = 0;
-				while(S5 == 0);
-				L5 = 1;
-			}
-		}
-	}
-	if(S4 == 0)
-	{
-		DelayK(100);
-		if(S4 == 0)
-		{
-			if(stat_k == 1)
-			{
-				L4 = 0;
-				while(S4 == 0);
-				L4 = 1;
-			}
-			else if(stat_k == 2)
-			{
-				L6 = 0;
-				while(S4 == 0);
-				L6 = 1;
-			}
-		}
-	}
+        if(S7 == 0)
+        {
+                Delay(200);
+                if(S7 == 0)
+                {
+                        while(S7 == 0);
+                        ShowKeyNum(SMG_HaveDot[1]);
+                }
+        }
+        if(S6 == 0)
+        {
+                Delay(200);
+                if(S6 == 0)
+                {
+                        while(S6 == 0);
+                        ShowKeyNum(SMG_HaveDot[2]);
+                }
+        }
+        if(S5 == 0)
+        {
+                Delay(200);
+                if(S5 == 0)
+                {
+                        while(S5 == 0);
+                        ShowKeyNum(SMG_HaveDot[3]);
+                }
+        }
+        if(S4 == 0)
+        {
+                Delay(200);
+                if(S4 == 0)
+                {
+                        while(S4 == 0);
+                        ShowKeyNum(SMG_HaveDot[4]);
+                }
+        }
 }
                           
 void main()
 {
-	Init_138(4);
         while(1)
         {
-			ScanKeys();
+                ScanKeys();
         }
 }
